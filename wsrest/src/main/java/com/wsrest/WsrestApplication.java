368784a66c5c7f3c307006ec1f847609357c1f0c
Juan Carlos <juank20097@gmail.com>
2022-11-08 19:38:01 -0500

    Creation of Backend Project

---
+++ wsrest/src/main/java/com/wsrest/WsrestApplication.java
@@ -0,0 +1,13 @@
+package com.wsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsrestApplication.class, args);
+	}
+
+}
---
+++ wsrest/src/test/java/com/wsrest/WsrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
